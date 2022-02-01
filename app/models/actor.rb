7a02bb594a104b845a2adb5e34e5456edfcabeb1
mcelhinneyk <mr.mcelhinney@gmail.com>
2022-01-31 19:54:15 -0800

    commit tree 1cd3aa96c8c38c002ef8580c6c211101d55c9716

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    actor_roles = self.characters
+    actor_roles.map {|character| "#{character.name} - #{character.show.name}"}
+  end
+
 end
